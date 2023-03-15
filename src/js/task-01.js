38dfa23cec36680086664ad8eb1fe6113251f530
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-03-15 23:51:15 +0200

    done tasks 2 and 3.

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -8,8 +8,8 @@ function categoriesCounter(rootEl) {
 function printCategoriesContent(itemEls) {
   itemEls.forEach((element) => {
     console.log("");
-    console.log(`Category: ${element.querySelector("h2").textContent};`);
-    console.log(`Elements: ${element.querySelectorAll("li").length};`);
+    console.log(`Category: ${element.querySelector("h2").textContent}`);
+    console.log(`Elements: ${element.querySelectorAll("li").length}`);
   });
 }
 
