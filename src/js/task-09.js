abbdf4a2dc3a4743aadbea691c1b73813781bc57
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-03-19 12:01:51 +0200

    done task 7 , 8 , 9 and 10.

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onBtnClick() {
+  let randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+}
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", onBtnClick);
